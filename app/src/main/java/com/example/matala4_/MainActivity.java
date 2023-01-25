012a185d0545b0ebe555cd690a57cb2e0019fa39
kr9277 <kr9277@bs.amalnet.k12.il>
2023-01-25 14:27:35 +0200

    matala4

---
+++ app/src/main/java/com/example/matala4_/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.matala4_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    ImageView iv;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random rnd = new Random();
+                int num = rnd.nextInt(3) + 1;
+                button.setText("Click number: " + num);
+                switch (num) {
+                    case 1:
+                        iv.setImageResource(R.drawable.one);
+                        break;
+                    case 2:
+                        iv.setImageResource(R.drawable.cattow);
+                        break;
+                    case 3:
+                        iv.setImageResource(R.drawable.dogthree);
+                        break;
+                }
+            }
+        });
+    }
+}
